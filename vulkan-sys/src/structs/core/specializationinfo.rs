9d6afc3209f4554d3060e8372e489700d4beee21
hodasemi <michaelh.95@t-online.de>
2019-11-13 15:01:29 +0100

    Easy specialization info ctor

--- vulkan-sys/src/structs/core/specializationinfo.rs
+++ vulkan-sys/src/structs/core/specializationinfo.rs
@@ -10,3 +10,14 @@ pub struct VkSpecializationInfo {
     pub dataSize: usize,
     pub pData: *const c_void,
 }
+
+impl VkSpecializationInfo {
+    pub fn new<T>(data: &T, map_entries: &[VkSpecializationMapEntry]) -> Self {
+        VkSpecializationInfo {
+            mapEntryCount: map_entries.len() as u32,
+            pMapEntries: map_entries.as_ptr(),
+            dataSize: std::mem::size_of::<T>(),
+            pData: data as *const T as *const c_void,
+        }
+    }
+}
