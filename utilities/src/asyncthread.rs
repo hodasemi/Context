2e9e90a41883ae6db3fc1d4c2126cf25ce736c18
hodasemi <michaelh.95@t-online.de>
2019-12-28 12:02:55 +0100

    Split get and check of AsyncThread

--- utilities/src/asyncthread.rs
+++ utilities/src/asyncthread.rs
@@ -1,16 +1,21 @@
 //! Asynchronous thread to check if result is returned (non-blocking)
 
-use std::cell::Cell;
+use crate::{
+    create_error,
+    errortype::{UtilError, VerboseResult},
+};
+
+use std::cell::RefCell;
 use std::sync::mpsc;
 use std::thread;
 
 /// Asynchronous thread handle
-pub struct AsyncThread<T: Copy + Send + 'static> {
+pub struct AsyncThread<T: Send + 'static> {
     receiver: mpsc::Receiver<T>,
-    result: Cell<Option<T>>,
+    result: RefCell<Option<T>>,
 }
 
-impl<T: Copy + Send + 'static> AsyncThread<T> {
+impl<T: Send + 'static> AsyncThread<T> {
     /// Spawns a thread
     ///
     /// # Arguments
@@ -31,24 +36,54 @@ impl<T: Copy + Send + 'static> AsyncThread<T> {
 
         AsyncThread {
             receiver,
-            result: Cell::new(None),
+            result: RefCell::new(None),
         }
     }
 
     /// Checks if the thread is already returned
     /// Returns the Some(result) if the thread has finished its work,
     /// otherwise None
-    pub fn check(&self) -> Option<T> {
-        if self.result.get().is_none() {
-            match self.receiver.try_recv() {
-                Ok(result) => {
-                    self.result.set(Some(result));
-                    return self.result.get();
-                }
-                Err(_) => return None,
-            }
-        }
+    pub fn check(&self) -> VerboseResult<bool> {
+        let mut result = self.result.try_borrow_mut()?;
 
-        self.result.get()
+        match result.as_ref() {
+            Some(_) => Ok(true),
+            None => match self.receiver.try_recv() {
+                Ok(res) => {
+                    *result = Some(res);
+
+                    Ok(true)
+                }
+                Err(_) => Ok(false),
+            },
+        }
+    }
+
+    /// consumes the result
+    pub fn take(&self) -> VerboseResult<T> {
+        let mut result = self.result.try_borrow_mut()?;
+
+        if result.is_some() {
+            // actually safe to not panic, since we just checked
+            let res = result.take().unwrap();
+
+            Ok(res)
+        } else {
+            create_error!("no result present!")
+        }
+    }
+}
+
+impl<T: Send + 'static> AsyncThread<T>
+where
+    T: Clone,
+{
+    pub fn get(&self) -> VerboseResult<T> {
+        let result = self.result.try_borrow()?;
+
+        match result.as_ref() {
+            Some(res) => Ok(res.clone()),
+            None => create_error!("no result present!"),
+        }
     }
 }
