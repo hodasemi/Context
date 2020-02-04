fa1cd434056a2fed721f11109df22401e1a3fdd2
hodasemi <michaelh.95@t-online.de>
2020-02-04 10:09:06 +0100

    Use builder pattern for flexibility

--- context/src/utils/single_submit.rs
+++ context/src/utils/single_submit.rs
@@ -3,36 +3,69 @@ use crate::prelude::*;
 use std::sync::{Arc, Mutex};
 use std::time::Duration;
 
-pub struct SingleSubmit;
+pub struct SingleSubmit<'a, F, T>
+where
+    F: FnOnce(&Arc<CommandBuffer>) -> VerboseResult<T>,
+{
+    command_buffer: &'a Arc<CommandBuffer>,
+    queue: &'a Arc<Mutex<Queue>>,
+    f: F,
 
-impl SingleSubmit {
-    pub fn submit<F, T>(
-        command_buffer: &Arc<CommandBuffer>,
-        queue: &Arc<Mutex<Queue>>,
+    timeout: Option<Duration>,
+}
+
+impl<'a, F, T> SingleSubmit<'a, F, T>
+where
+    F: FnOnce(&Arc<CommandBuffer>) -> VerboseResult<T>,
+{
+    pub fn builder(
+        command_buffer: &'a Arc<CommandBuffer>,
+        queue: &'a Arc<Mutex<Queue>>,
         f: F,
-        timeout: Duration,
-    ) -> VerboseResult<T>
-    where
-        F: FnOnce(&Arc<CommandBuffer>) -> VerboseResult<T>,
-    {
-        command_buffer.begin(VkCommandBufferBeginInfo::new(
+    ) -> Self {
+        SingleSubmit {
+            command_buffer,
+            queue,
+            f,
+
+            timeout: None,
+        }
+    }
+
+    pub fn wait_for_timeout(mut self, timeout: Duration) -> Self {
+        self.timeout = Some(timeout);
+
+        self
+    }
+
+    pub fn submit(self) -> VerboseResult<T> {
+        self.command_buffer.begin(VkCommandBufferBeginInfo::new(
             VK_COMMAND_BUFFER_USAGE_ONE_TIME_SUBMIT_BIT,
         ))?;
 
-        let result = f(command_buffer)?;
+        let result = (self.f)(&self.command_buffer)?;
 
-        command_buffer.end()?;
+        self.command_buffer.end()?;
 
-        let submit = SubmitInfo::default().add_command_buffer(command_buffer);
-        let fence = Fence::builder().build(command_buffer.device().clone())?;
+        let submit = SubmitInfo::default().add_command_buffer(self.command_buffer);
+        let queue_lock = self.queue.lock()?;
 
-        let queue_lock = queue.lock()?;
+        match self.timeout {
+            Some(timeout) => {
+                let fence = Fence::builder().build(self.command_buffer.device().clone())?;
 
-        queue_lock.submit(Some(&fence), &[submit])?;
+                queue_lock.submit(Some(&fence), &[submit])?;
 
-        command_buffer
-            .device()
-            .wait_for_fences(&[&fence], true, timeout)?;
+                self.command_buffer
+                    .device()
+                    .wait_for_fences(&[&fence], true, timeout)?;
+            }
+            None => {
+                queue_lock.submit(None, &[submit])?;
+
+                queue_lock.wait_idle()?;
+            }
+        }
 
         Ok(result)
     }
